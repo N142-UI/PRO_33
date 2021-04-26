d1c1f63013872376e9f83f84ef7e26176dcf9137
N142-UI <75881981+N142-UI@users.noreply.github.com>
2021-04-26 20:32:58 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,56 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies =  Matter.Bodies;
+const Constraint = Matter.Constraint;
+var bg,ground,gimg;
+var runner,runnerImg;
+var ice=[];
+var maxSnow=100;
+
+function preload(){
+  bg=loadImage("snow3.jpg");
+}
+
+function setup() {
+  createCanvas(1300,600);
+  
+  engine=Engine.create();
+  world= engine.world;
+  
+
+
+ground=createSprite(650,670);
+ground.addImage(gimg);
+ground.scale=3.2;
+ground.velocityX=-10;
+
+runner=createSprite(150,480); 
+
+if(frameCount % 275 === 0){
+  for(var i=0; i<maxSnow; i++){
+  ice.push(new Snow(random(0,1350), random(0,50)));
+  }
+  }
+
+
+}
+
+function draw() {
+  background(bg);  
+  Engine.update(engine);
+
+  runner.collide(ground);
+
+  for(var i = 0;i < maxSnow; i++){
+    ice[i].display();
+    ice[i].changePosition();
+    }    
+    
+
+
+ground.display();
+
+  
+  drawSprites();
+
+}
